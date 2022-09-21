a0b8e05a41b8890b29f8ff110015e80b8ebdf0f2
theCarlG <me@carlg.tech>
2022-09-21 20:53:08 +0200

    Optional beta password

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,19 +16,24 @@ fi
 
 
 if [ ! -z "$STEAM_BETA_BRANCH" ]; then
-	echo "Loading Steam Beta Branch"
-	bash "${STEAMCMD_DIR}/steamcmd.sh" +force_install_dir "${STEAM_APP_DIR}" \
-					+login anonymous \
-					+app_update "${STEAM_BETA_APP}" \
-					-beta "${STEAM_BETA_BRANCH}" \
-					-betapassword "${STEAM_BETA_PASSWORD}" \
-					+quit
+
+    PW=""
+    if [ ! -z "$STEAM_BETA_PASSWORD" ]; then
+        PW="-betapassword \"${STEAM_BETA_PASSWORD}\""
+    fi
+
+    echo "Loading Steam Beta Branch"
+    bash "${STEAMCMD_DIR}/steamcmd.sh" +force_install_dir "${STEAM_APP_DIR}" \
+                    +login anonymous \
+                    +app_update "${STEAM_BETA_APP}" \
+                    -beta "${STEAM_BETA_BRANCH}" $PW \
+                    +quit
 else
-	echo "Loading Steam Release Branch"
-	bash "${STEAMCMD_DIR}/steamcmd.sh" +force_install_dir "${STEAM_APP_DIR}" \
-					+login anonymous \
-					+app_update "${STEAM_APP_ID}" \
-					+quit
+    echo "Loading Steam Release Branch"
+    bash "${STEAMCMD_DIR}/steamcmd.sh" +force_install_dir "${STEAM_APP_DIR}" \
+                    +login anonymous \
+                    +app_update "${STEAM_APP_ID}" \
+                    +quit
 fi
 
 exec "$@"
