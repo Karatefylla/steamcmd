8cb43e43fb3e02b4c16b0862e2df2f7cf7270d94
theCarlG <me@carlg.tech>
2022-10-01 10:58:30 +0200

    fix: fixed typo in entyrpoint.sh, added DEBUG env var

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
+if [ -z "$DEBUG" ]; then
+    set -x
+fi
+
 if [ -z "$STEAM_APP_DIR" ]; then
     echo "STEAM_APP_DIR not set"
     exit 1
@@ -25,14 +29,16 @@ if [ ! -z "$STEAM_BETA_BRANCH" ]; then
     echo "Loading Steam Beta Branch"
     bash "${STEAMCMD_DIR}/steamcmd.sh" +force_install_dir "${STEAM_APP_DIR}" \
                     +login anonymous \
-                    +app_update "${STEAM_BETA_APP}" \
+                    +app_update "${STEAM_APP_ID}" \
                     -beta "${STEAM_BETA_BRANCH}" $PW \
+                    validate \
                     +quit
 else
     echo "Loading Steam Release Branch"
     bash "${STEAMCMD_DIR}/steamcmd.sh" +force_install_dir "${STEAM_APP_DIR}" \
                     +login anonymous \
                     +app_update "${STEAM_APP_ID}" \
+                    validate \
                     +quit
 fi
 
